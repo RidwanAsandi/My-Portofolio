db5f422957b2ad5a26572a03f4231d7846d14f5c
RidwanAsandi <ridwanasandi09@gmail.com>
2024-03-22 15:42:50 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import { Component } from "react";
+import Header from "./Components/Header.js"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {},
+    };
+
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: "./resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({
+          resumeData: data,
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+      </div>
+    )
+  }
+}
+
+export default App;
